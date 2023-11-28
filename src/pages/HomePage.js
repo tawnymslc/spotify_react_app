3adc4563a1f8e73d2007490d2e9f30b6b534aca9
Tawny Mathi <tawnymslc@gmail.com>
2023-11-27 19:39:50 -0700

    updated UI cards, background, search, text, etc

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,17 +3,15 @@ import { Button, Container, Col, Row, Card,
     CardBody, 
     CardImg, 
     CardText, 
-    CardTitle, 
-    CardFooter } from 'reactstrap';
+ } from 'reactstrap';
 import axios from "axios";
+import spotify_bkg from '../img/spotify_bkg.jpg'
 
 const HomePage = () => {
 
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState(null);
     const [tracks, setTracks] = useState(null);
-
-    const API_ARTIST_ID = "https://api.spotify.com/v1/search?";
 
     const handleInputChange = (event) => {
         setSearchQuery(event.target.value);
@@ -23,11 +21,11 @@ const HomePage = () => {
         event.preventDefault();
     
         try{
-            const response = await axios.post('http://localhost:4244/get-access-token');
+            const response = await axios.post(process.env.REACT_APP_GET_TOKEN_URL);
             const accessToken = response.data.accessToken;
             console.log('Received access token:', accessToken);
     
-            const searchResponse = await axios.get(API_ARTIST_ID, {
+            const searchResponse = await axios.get("https://api.spotify.com/v1/search?", {
                 params: { q: searchQuery, type: "artist" },
                 headers: {
                     'Authorization': `Bearer ${accessToken}`
@@ -52,8 +50,10 @@ const HomePage = () => {
 
     return(
         <>
-        <Container className="container-fluid" style={{ backgroundColor: 'black', height: '100vh' }}>
-            <Row sm={11} className="justify-content-center">
+        <div 
+        style={{  backgroundImage: `url(${require('../img/wp2775382.jpg')})`}}>
+        <Container className="container-fluid spotify-container">
+            <Row sm={11} className="justify-content-center" style={{marginBottom: "40px"}}>
                 <Col xs='auto' className="mx-auto" style={{ marginTop: '40px' }}>
                     <form onSubmit={handleSearchSubmit}>
                         <input
@@ -61,35 +61,32 @@ const HomePage = () => {
                             className="custom-search-bar" 
                             value={searchQuery}
                             onChange={handleInputChange}
+                            style={{ textAlign: 'center', verticalAlign: 'middle' }}
                         />
                         <Button 
                             type='submit' 
-                            style={{ backgroundColor: 'rgb(77, 72, 242)', marginBottom: '4px' }}>
+                            style={{ backgroundColor: 'rgb(43, 92, 36)', color: 'white', lineHeight: "16px", display: 'inline-block', textAlign: 'center', fontWeight: 'bold'}}>
                             Search
                         </Button>
                     </form>
                 </Col>
             </Row>
-            <Container style={{ marginTop: '30px' }}>
                 <Row className="justify-content-center">
                 {tracks && tracks.map((track, index) => (
-                <Col md={3} className="m-4 text-center">
-                    <Card className='domain-card'>
-                        <CardBody className='domain-card-body'>
-                            <CardImg 
-                            />
-                            <CardTitle>{track.name}</CardTitle>
-                            <CardText> </CardText>
+                <Col md={3} className="m-4 text-center" key={index}>
+                    <Card className='spotify-card' style={{backgroundColor: 'black'}}>
+                    <CardBody>
+                        <CardImg src={track.album.images[1].url} style={{padding: "-25px"}}></CardImg>
+                        <CardText style={{ backgroundColor: "black", color: "white", display: 'flex', justifyContent: 'center', border: '5px', marginTop: '10px', fontWeight: 'bold' }}>
+                            {track.name}
+                        </CardText>
                         </CardBody>
-                        <CardText style={{ marginTop: '15px', fontWeight: 'bold'}}></CardText>
-                        <CardFooter style={{ display: 'flex', justifyContent: 'center' }}>
-                        </CardFooter>
                     </Card>
                 </Col>
                 ))}
             </Row>
-            </Container>
         </Container>
+        </div>
         </>
     );
 };
